71fadc8255460461dc53e068730a31c60095b0f7
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-11-18 14:15:03 +0900

    conmmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
+const Box = styled.div`
+  width: ${(props) => props.width || `150px`};
+  height: ${(props) => props.height || `150px`};
+  margin: 10px;
+  background-color: ${(props) =>
+    props.isTheme
+      ? props.theme.mainThemeColor
+      : props.isSubTheme
+      ? props.theme.subThemeColor
+      : `red`};
+  box-shadow: ${(props) => (props.isShadow ? props.theme.boxShadow : ``)};
+
+  transition: 0.5s;
+
+  ${(props) => (props.isHover ? `&:hover {opacity: 0.5;}` : ``)}
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box width={`200px`} height={`200px`}></Box>
+      <Box width={`100%`} height={`40px`} isTheme={true} isShadow={true}></Box>
+      <Box isSubTheme={true} isHover={true}></Box>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Styles/Theme.js
@@ -0,0 +1,12 @@
+export default {
+  mainThemeColor: `#2c2c54`,
+  subThemeColor: `#40407a`,
+
+  simpleBorder: `1px solid #999`,
+  boldBorder: `2px solid #999`,
+
+  boxShadow: `3px 3px 3px #777`,
+
+  whiteColor: `#fff`,
+  blackColor: `#0b0b0b`,
+};
